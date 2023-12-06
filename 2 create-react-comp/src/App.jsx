887a85522592dd95b6179920199c169f15b1e872
snapyy <210510113165@paruluniversity.ac.in>
2023-12-06 19:44:07 +0530

    Add files via upload

---
+++ 2 create-react-comp/src/App.jsx
@@ -0,0 +1,19 @@
+  import Button from "./Button";
+  import Hello from "./Hello";
+  import Random from "./random";
+function App(){
+return <div><h1>
+Hello world!
+</h1>
+
+<Hello></Hello>
+<Button></Button>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+<Random></Random>
+
+</div>
+}
+export default App;
---
+++ 2 create-react-comp/src/Hello.jsx
@@ -0,0 +1,13 @@
+function Hello(){
+  let myName='Gaku';
+  let number =3165;
+  let fullName=()=>{
+    return 'Gta is ❣️❣️'
+  }
+return <h3>
+  Hello this is the future speaking {fullName()}. I am your master {myName}
+  Here is my number {number}
+</h3>
+}
+
+export default Hello;
