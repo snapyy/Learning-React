1fbf2369cd8d44875631705bd9087c79804b05fb
snapyy <210510113165@paruluniversity.ac.in>
2023-12-09 20:53:07 +0530

    Add files via upload

---
+++ Mini-Projects/1-todo-app-version-one/src/components/TodoItem1.jsx
@@ -0,0 +1,18 @@
+function TodoItem1() {
+  let todoName = "Buy Milk";
+  let todoDate = "4/10/2023<";
+  return (
+    <div class="container text-center">
+      <div class="row gaku-row">
+        <div class="col-6">{todoName}</div>
+        <div class="col-4">{todoDate}</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger gaku-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default TodoItem1;
