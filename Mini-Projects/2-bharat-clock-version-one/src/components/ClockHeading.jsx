08833cd2f8905017e47a8f4fdd342302c8d836fe
snapyy <210510113165@paruluniversity.ac.in>
2023-12-10 19:27:40 +0530

    Add files via upload

---
+++ Mini-Projects/2-bharat-clock-version-one/src/components/ClockHeading.jsx
@@ -0,0 +1,4 @@
+function ClockHeading() {
+  return <h1 className="fw-bold">Bharat Clock</h1>;
+}
+export default ClockHeading;
---
+++ Mini-Projects/2-bharat-clock-version-one/src/components/ClockSlogan.jsx
@@ -0,0 +1,8 @@
+function ClockSlogan() {
+  return (
+    <p className="lead">
+      This is the clock that shows the time in Bharat at all times.
+    </p>
+  );
+}
+export default ClockSlogan;
