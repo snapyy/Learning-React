5a25986669445d28339e38e5071f55fe14c6b247
snapyy <210510113165@paruluniversity.ac.in>
2023-12-19 16:20:56 +0530

    Add files via upload

--- 4 Fragments/learning-fragment/src/App.jsx
+++ 4 Fragments/learning-fragment/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
 function App() {
+  // let foodItems = [];
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk", "Ghee"];
+
   return (
     <>
       <h1>Healthy Food</h1>
+      {foodItems.length === 0 && <h3>I am still hungry</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
