196d07a42c5e82cefbe3b72698b594757ba82072
snapyy <210510113165@paruluniversity.ac.in>
2023-12-25 00:01:53 +0530

    Update ButtonsContainer.jsx

--- Mini-Projects/4-calculator-version-1/calculator/src/components/ButtonsContainer.jsx
+++ Mini-Projects/4-calculator-version-1/calculator/src/components/ButtonsContainer.jsx
@@ -1,11 +1,29 @@
 import styles from "./ButtonContainer.module.css";
 const ButtonsContainer = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div className={styles.buttonsContainer}>
-      <button className={styles.button}>C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
+      {buttonNames.map((buttonNames) => (
+        <button className={styles.button}>{buttonNames}</button>
+      ))}
     </div>
   );
 };
