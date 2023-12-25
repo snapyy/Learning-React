31301e5ee54fd6fde4810922983c1c3f212171b5
snapyy <210510113165@paruluniversity.ac.in>
2023-12-25 21:06:26 +0530

    App.jsx

--- 4 Fragments/learning-fragment/src/App.jsx
+++ 4 Fragments/learning-fragment/src/App.jsx
@@ -3,15 +3,22 @@ import "./App.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 function App() {
   // let foodItems = [];
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad", "Milk", "Ghee"];
-
+  let textToShow = "Food Item Entered by user";
+  const handleOnchange = (event) => {
+    console.log(event.target.value);
+    textToShow = event.target.value;
+  };
   return (
     <>
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage items={foodItems}> </ErrorMessage>
+        <FoodInput handleOnChange={handleOnchange}></FoodInput>
+        <p>{textToShow}</p>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
 
