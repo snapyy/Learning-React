dd22f331fb7cc6e02161035e50b518f362a4cc79
snapyy <210510113165@paruluniversity.ac.in>
2023-12-26 20:16:24 +0530

    Update FoodItems.jsx

--- 4 Fragments/learning-fragment/src/components/FoodItems.jsx
+++ 4 Fragments/learning-fragment/src/components/FoodItems.jsx
@@ -1,12 +1,20 @@
 import Item from "./Item";
+import { useState } from "react";
 const FoodItems = ({ items }) => {
+  let [activeItems, setActiveItems] = useState([]);
+
+  let onBuyButton = (item, event) => {
+    let newItems = [...activeItems, item];
+    setActiveItems(newItems);
+  };
   return (
     <ul className="list-group">
       {items.map((item) => (
         <Item
           key={item}
           foodItem={item}
-          handleBuyButtonClicked={() => console.log(`${item} bought`)}
+          bought={activeItems.includes(item)}
+          handleBuyButtonClicked={(event) => onBuyButton(item, event)}
         ></Item>
       ))}
     </ul>
