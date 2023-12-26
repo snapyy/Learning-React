6065020d387b5575c415c97072465255a56bcfe5
snapyy <210510113165@paruluniversity.ac.in>
2023-12-26 20:17:05 +0530

    Update Item.jsx

--- 4 Fragments/learning-fragment/src/components/Item.jsx
+++ 4 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,7 +1,11 @@
 import styles from "./Item.module.css";
-const Item = ({ foodItem, handleBuyButtonClicked }) => {
+const Item = ({ foodItem, bought, handleBuyButtonClicked }) => {
   return (
-    <li className={`${styles["theitem"]} list-group-item theitem `}>
+    <li
+      className={`${styles["theitem"]} list-group-item theitem  ${
+        bought && "active"
+      }`}
+    >
       <span className={styles["thespan"]}>{foodItem}</span>
       <button
         className={`${styles.button} btn btn-info`}
