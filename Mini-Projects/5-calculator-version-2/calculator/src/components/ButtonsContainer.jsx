3eb42e426e972a8c8a98ded069239c9841e94012
snapyy <210510113165@paruluniversity.ac.in>
2023-12-28 21:06:49 +0530

    Add files via upload

---
+++ Mini-Projects/5-calculator-version-2/calculator/src/components/ButtonsContainer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./ButtonContainer.module.css";
+
+const ButtonsContainer = ({ onButtonClick }) => {
+  // Destructure onButtonClick from props
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <div className={styles.buttonsContainer}>
+      {buttonNames.map((buttonName, index) => (
+        <button
+          key={index}
+          className={styles.button}
+          onClick={() => onButtonClick(buttonName)}
+        >
+          {buttonName}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ButtonsContainer;
