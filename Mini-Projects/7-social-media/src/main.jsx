03c5cadf017e43f96f3e17070af57179a46ca296
snapyy <210510113165@paruluniversity.ac.in>
2024-01-09 22:44:12 +0530

    Update main.jsx

--- Mini-Projects/7-social-media/src/main.jsx
+++ Mini-Projects/7-social-media/src/main.jsx
@@ -1,18 +1,9 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Sidebar from "./components/Sidebar";
-function App() {
-  return (
-    <div className="app-container">
-      <Sidebar></Sidebar>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-      <div className="content">
-        <Header></Header>
-        <Footer></Footer>
-      </div>
-    </div>
-  );
-}
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
