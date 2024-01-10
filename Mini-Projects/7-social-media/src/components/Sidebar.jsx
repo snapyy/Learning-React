1fbf86487e427eb35dc3de185e3bf4efc28fe2c7
snapyy <210510113165@paruluniversity.ac.in>
2024-01-10 21:31:19 +0530

    Update Sidebar.jsx

--- Mini-Projects/7-social-media/src/components/Sidebar.jsx
+++ Mini-Projects/7-social-media/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-const Sidebar = () => {
+const Sidebar = ({ selectedTag }) => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -17,7 +17,13 @@ const Sidebar = () => {
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTag === "Home" && "active"
+            }`}
+            aria-current="page"
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
@@ -25,7 +31,12 @@ const Sidebar = () => {
           </a>
         </li>
         <li>
-          <a href="#" className="nav-link text-white">
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTag === "Create Post" && "active"
+            }`}
+          >
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
